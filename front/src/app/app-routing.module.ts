e23fcee4ba602925f59b27745ac7ce1a19d7d328
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-05-10 12:20:58 -0500

    Resolviendo problemas

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+<<<<<<< HEAD
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+=======
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { LoginComponent } from './auth/login/login.component';
+
+export const AppRoutes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },{  path:'home',loadChildren: () => import('./home/home.module').then( m => m.HomeModule )},
+  { path: '**', redirectTo: 'home' }
+];
+@NgModule({
+  imports: [
+    AuthModule,
+    RouterModule.forRoot(AppRoutes)],
+>>>>>>> 8432047 (Creando login)
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AuthModule } from './auth/auth.module'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AuthModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
