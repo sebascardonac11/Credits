21ccc327963475e6e9c5f462d19414c43cd84185
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-05-10 12:27:23 -0500

    Update routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-<<<<<<< HEAD
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-=======
 import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { LoginComponent } from './auth/login/login.component';
@@ -23,7 +17,6 @@ export const AppRoutes: Routes = [
   imports: [
     AuthModule,
     RouterModule.forRoot(AppRoutes)],
->>>>>>> 8432047 (Creando login)
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
